f14b8610b785a1117327ad326562259260e93e71
Rekishii Lucy <rekishiilucy658@gmail.com>
2025-09-10 15:39:06 +0700

    update style untuk profil dan tabel lihat lamaran

--- pelamar/profile.php
+++ pelamar/profile.php
@@ -68,6 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../assets/css/pages.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/pofile.css">
 </head>
 
 <body>
